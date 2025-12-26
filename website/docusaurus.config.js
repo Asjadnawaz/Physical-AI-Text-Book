0373f08be396e21290bb9e74250eb0dd10ccb29d
Asjad Nawaz Khan <asjadnawaz2002@gmail.com>
2025-12-26 15:49:24 +0500

    Added URL to docasaurus.config

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -12,7 +12,7 @@ const config = {
   favicon: 'img/robot-icon.png',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://physical-ai-text-book-lovat.vercel.app/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
